6665f6a710952f9e78442ea0ddb060aff80180f6
InIn Devops <devops-jenkins@inin.com>
2017-04-04 17:05:03 +0000

    2.0.24

--- build/lib/purecloudplatformclientv2/models/outbound_route_base_entity_listing.rb
+++ build/lib/purecloudplatformclientv2/models/outbound_route_base_entity_listing.rb
@@ -30,11 +30,11 @@ module PureCloud
 
     attr_accessor :first_uri
 
-    attr_accessor :previous_uri
-
     attr_accessor :next_uri
 
     attr_accessor :last_uri
+
+    attr_accessor :previous_uri
 
     attr_accessor :page_count
 
@@ -47,9 +47,9 @@ module PureCloud
         :'total' => :'total',
         :'self_uri' => :'selfUri',
         :'first_uri' => :'firstUri',
-        :'previous_uri' => :'previousUri',
         :'next_uri' => :'nextUri',
         :'last_uri' => :'lastUri',
+        :'previous_uri' => :'previousUri',
         :'page_count' => :'pageCount'
       }
     end
@@ -63,9 +63,9 @@ module PureCloud
         :'total' => :'Integer',
         :'self_uri' => :'String',
         :'first_uri' => :'String',
-        :'previous_uri' => :'String',
         :'next_uri' => :'String',
         :'last_uri' => :'String',
+        :'previous_uri' => :'String',
         :'page_count' => :'Integer'
       }
     end
@@ -104,16 +104,16 @@ module PureCloud
         self.first_uri = attributes[:'firstUri']
       end
 
-      if attributes.has_key?(:'previousUri')
-        self.previous_uri = attributes[:'previousUri']
-      end
-
       if attributes.has_key?(:'nextUri')
         self.next_uri = attributes[:'nextUri']
       end
 
       if attributes.has_key?(:'lastUri')
         self.last_uri = attributes[:'lastUri']
+      end
+
+      if attributes.has_key?(:'previousUri')
+        self.previous_uri = attributes[:'previousUri']
       end
 
       if attributes.has_key?(:'pageCount')
@@ -145,9 +145,9 @@ module PureCloud
           total == o.total &&
           self_uri == o.self_uri &&
           first_uri == o.first_uri &&
-          previous_uri == o.previous_uri &&
           next_uri == o.next_uri &&
           last_uri == o.last_uri &&
+          previous_uri == o.previous_uri &&
           page_count == o.page_count
     end
 
@@ -160,7 +160,7 @@ module PureCloud
     # Calculates hash code according to all attributes.
     # @return [Fixnum] Hash code
     def hash
-      [entities, page_size, page_number, total, self_uri, first_uri, previous_uri, next_uri, last_uri, page_count].hash
+      [entities, page_size, page_number, total, self_uri, first_uri, next_uri, last_uri, previous_uri, page_count].hash
     end
 
     # build the object from hash
--- build/lib/purecloudplatformclientv2/version.rb
+++ build/lib/purecloudplatformclientv2/version.rb
@@ -15,5 +15,5 @@ Terms of Service: https://developer.mypurecloud.com/tos
 =end
 
 module PureCloud
-  VERSION = "2.0.23"
+  VERSION = "2.0.24"
 end
