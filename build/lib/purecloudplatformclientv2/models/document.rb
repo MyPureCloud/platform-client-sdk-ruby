52b22352a42cc3e4229fe3be73b9d0da91ef769e
InIn Devops <devops-jenkins@inin.com>
2017-04-04 17:51:09 +0000

    2.0.30

--- build/lib/purecloudplatformclientv2/models/document.rb
+++ build/lib/purecloudplatformclientv2/models/document.rb
@@ -79,9 +79,9 @@ module PureCloud
 
     attr_accessor :sharing_status
 
-    attr_accessor :sharing_uri
-
     attr_accessor :download_sharing_uri
+
+    attr_accessor :sharing_uri
 
     # The URI for this object
     attr_accessor :self_uri
@@ -117,8 +117,8 @@ module PureCloud
         :'lock_info' => :'lockInfo',
         :'acl' => :'acl',
         :'sharing_status' => :'sharingStatus',
-        :'sharing_uri' => :'sharingUri',
         :'download_sharing_uri' => :'downloadSharingUri',
+        :'sharing_uri' => :'sharingUri',
         :'self_uri' => :'selfUri'
       }
     end
@@ -154,8 +154,8 @@ module PureCloud
         :'lock_info' => :'LockInfo',
         :'acl' => :'Array<String>',
         :'sharing_status' => :'String',
-        :'sharing_uri' => :'String',
         :'download_sharing_uri' => :'String',
+        :'sharing_uri' => :'String',
         :'self_uri' => :'String'
       }
     end
@@ -290,12 +290,12 @@ module PureCloud
         self.sharing_status = attributes[:'sharingStatus']
       end
 
-      if attributes.has_key?(:'sharingUri')
-        self.sharing_uri = attributes[:'sharingUri']
-      end
-
       if attributes.has_key?(:'downloadSharingUri')
         self.download_sharing_uri = attributes[:'downloadSharingUri']
+      end
+
+      if attributes.has_key?(:'sharingUri')
+        self.sharing_uri = attributes[:'sharingUri']
       end
 
       if attributes.has_key?(:'selfUri')
@@ -391,8 +391,8 @@ module PureCloud
           lock_info == o.lock_info &&
           acl == o.acl &&
           sharing_status == o.sharing_status &&
-          sharing_uri == o.sharing_uri &&
           download_sharing_uri == o.download_sharing_uri &&
+          sharing_uri == o.sharing_uri &&
           self_uri == o.self_uri
     end
 
@@ -405,7 +405,7 @@ module PureCloud
     # Calculates hash code according to all attributes.
     # @return [Fixnum] Hash code
     def hash
-      [id, name, change_number, date_created, date_modified, date_uploaded, content_uri, workspace, created_by, uploaded_by, content_type, content_length, system_type, filename, page_count, read, caller_address, receiver_address, tags, tag_values, attributes, thumbnails, upload_status, upload_destination_uri, upload_method, lock_info, acl, sharing_status, sharing_uri, download_sharing_uri, self_uri].hash
+      [id, name, change_number, date_created, date_modified, date_uploaded, content_uri, workspace, created_by, uploaded_by, content_type, content_length, system_type, filename, page_count, read, caller_address, receiver_address, tags, tag_values, attributes, thumbnails, upload_status, upload_destination_uri, upload_method, lock_info, acl, sharing_status, download_sharing_uri, sharing_uri, self_uri].hash
     end
 
     # build the object from hash
--- build/lib/purecloudplatformclientv2/version.rb
+++ build/lib/purecloudplatformclientv2/version.rb
@@ -15,5 +15,5 @@ Terms of Service: https://developer.mypurecloud.com/tos
 =end
 
 module PureCloud
-  VERSION = "2.0.29"
+  VERSION = "2.0.30"
 end
