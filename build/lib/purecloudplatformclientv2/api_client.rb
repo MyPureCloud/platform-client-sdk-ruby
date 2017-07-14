1a2bab817d9c9868e3d36124fa6a43f220ba85a3
InIn Devops <devops-jenkins@inin.com>
2017-07-14 17:05:10 +0000

    13.1.0

--- build/lib/purecloudplatformclientv2/api_client.rb
+++ build/lib/purecloudplatformclientv2/api_client.rb
@@ -76,6 +76,7 @@ module PureCloud
       http_method = http_method.to_sym.downcase
 
       header_params = @default_headers.merge(opts[:header_params] || {})
+      header_params['purecloud-sdk'] = '13.1.0'
       query_params = opts[:query_params] || {}
       form_params = opts[:form_params] || {}
 
--- build/lib/purecloudplatformclientv2/version.rb
+++ build/lib/purecloudplatformclientv2/version.rb
@@ -15,5 +15,5 @@ Terms of Service: https://developer.mypurecloud.com/tos
 =end
 
 module PureCloud
-  VERSION = "13.0.0"
+  VERSION = "13.1.0"
 end
