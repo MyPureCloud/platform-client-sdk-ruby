96927b6eb6320ffa2b78e0908b0b8e619ad90212
PureCloud Jenkins <purecloud-jenkins@ininica.com>
2020-09-16 16:46:20 +0000

    97.0.19

--- build/lib/purecloudplatformclientv2/api_client.rb
+++ build/lib/purecloudplatformclientv2/api_client.rb
@@ -76,7 +76,7 @@ module PureCloud
       http_method = http_method.to_sym.downcase
 
       header_params = @default_headers.merge(opts[:header_params] || {})
-      header_params['purecloud-sdk'] = '97.0.18'
+      header_params['purecloud-sdk'] = '97.0.19'
       query_params = opts[:query_params] || {}
       form_params = opts[:form_params] || {}
 
